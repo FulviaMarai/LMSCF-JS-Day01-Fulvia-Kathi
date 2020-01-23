3b0ccd298c8e48e20dd0d375965e766ee128f6c5
Fulvia Marai <marai.fulvia@gmail.com>
2020-01-23 10:08:45 +0100

    basic exercises

---
+++ script.js
@@ -0,0 +1,7 @@
+	console.log("Hi my name is Martin");
+var age=25;
+	console.log(" and i am "+age+" years old.");
+
+var team=["Martin","Thomas","Peter","Mathias","Niki"];
+
+	console.log("THe most valuable player of the match is "+team[2]);
