e7f5d6d89a86bce23e8fe92ebcaf82cee85059c9
Kathi <katharina.hansi@gmx.at>
2020-01-23 14:15:36 +0100

    Extra Exercises

---
+++ extraex1.js
@@ -0,0 +1,13 @@
+// var text="Hey there, I am a javascript developer, and I live in vienna";
+// document.write(text.substring(10,38));
+
+// var text="Hey there, I am a javascript developer, and I live in vienna";
+// var temp= new Array();
+// temp=text.split(",");
+// document.write(temp.slice(1, 2));
+
+
+var fruits=["apple", "banana", "kiwi"];
+console.log(fruits.pop());
+console.log(fruits.push("orange", "strawberry", "kiwi"));
+document.write(fruits);
